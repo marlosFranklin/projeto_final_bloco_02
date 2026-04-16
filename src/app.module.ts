1d99f9bdbb0a55e0b4ce7f668d2c3db480ab5292
marlosFranklin <marlos-gama@hotmail.com>
2026-04-16 14:31:00 -0300

    feat: configura banco de dados da aplicacao

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,24 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { CategoriaModule } from './categoria/categoria.module';
+import { Categoria } from './categoria/entities/cateegoria.entity';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'root',
+      database: 'db_farmax',
+      entities: [Categoria],
+      synchronize: true,
+    }),
+    CategoriaModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
