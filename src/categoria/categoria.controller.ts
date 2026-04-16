58daea6ca0e83391b89fbde794dab90c3d8162b8
marlosFranklin <marlos-gama@hotmail.com>
2026-04-16 15:34:39 -0300

    feature: cria controller com todas as edpontis da api

--- src/categoria/categoria.controller.ts
+++ src/categoria/categoria.controller.ts
@@ -1,4 +1,46 @@
-import { Controller } from '@nestjs/common';
+import { Controller, HttpCode, HttpStatus, ParseIntPipe } from '@nestjs/common';
+import { Get, Post, Put, Delete, Param, Body } from '@nestjs/common';
+import { CategoriaService } from './categoria.service';
+import { Categoria } from './entities/cateegoria.entity';
 
 @Controller('categoria')
-export class CategoriaController {}
+export class CategoriaController {
+  constructor(private readonly categoriaService: CategoriaService) {}
+
+  @Get()
+  @HttpCode(HttpStatus.OK)
+  findAll() {
+    return this.categoriaService.findAll();
+  }
+
+  @Get(':id')
+  @HttpCode(HttpStatus.OK)
+  findOne(@Param('id') id: number) {
+    return this.categoriaService.findOne(id);
+  }
+
+  @Get('/ativo/:status')
+  findByativo(@Param('status') status: string) {
+    const ativo = status === 'true';
+
+    return this.categoriaService.findAllByStatus(ativo);
+  }
+
+  @Post()
+  @HttpCode(HttpStatus.CREATED)
+  create(@Body() categoria: Categoria) {
+    return this.categoriaService.create(categoria);
+  }
+
+  @Put(':id')
+  @HttpCode(HttpStatus.OK)
+  updtae(@Body() categoria: Categoria) {
+    return this.categoriaService.update(categoria);
+  }
+
+  @Delete(':id')
+  @HttpCode(HttpStatus.NO_CONTENT)
+  delete(@Param('id', ParseIntPipe) id: number) {
+    return this.categoriaService.delete(id);
+  }
+}
