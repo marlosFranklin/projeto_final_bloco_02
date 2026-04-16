7146f1701b2ad56af466321d953bbf0c523b62ee
marlosFranklin <marlos-gama@hotmail.com>
2026-04-16 15:35:06 -0300

    feature: cria modulo para gerenciamento do servico de categoria

--- src/categoria/categoria.module.ts
+++ src/categoria/categoria.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
 import { CategoriaService } from './categoria.service';
 import { CategoriaController } from './categoria.controller';
+import { Categoria } from './entities/cateegoria.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Categoria])],
+  controllers: [CategoriaController],
   providers: [CategoriaService],
-  controllers: [CategoriaController]
 })
 export class CategoriaModule {}
